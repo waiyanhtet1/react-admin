a0aa3802bbbcccc75a0e43b3e47de4a84e8ab43b
Wai Yan Htet <waiyanhtet1661390@gmail.com>
2023-09-10 21:05:22 +0630

    feat: add theme setup code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
